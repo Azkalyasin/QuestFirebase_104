49fb35fa7119c0a03442e19fdf529795fad7f330
Azkalyasin <yasinadzkiaazkal@gmail.com>
2026-01-02 16:42:25 +0700

    mainactivity

---
+++ app/src/main/java/com/example/p14/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.p14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.p14.ui.theme.P14Theme
+import com.example.p14.view.controller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P14Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
