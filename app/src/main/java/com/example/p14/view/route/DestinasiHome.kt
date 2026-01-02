4f7bf2dff50c82fc8055aff539c1d71d276e2cd1
Azkalyasin <yasinadzkiaazkal@gmail.com>
2026-01-02 16:37:45 +0700

    add Destinasihome

---
+++ app/src/main/java/com/example/p14/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.p14.view.route
+
+import com.example.p14.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
